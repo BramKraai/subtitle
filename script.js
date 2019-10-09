bc1ea773e8ae668d5bb5c92d2da67109120bbf98
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-10-09 13:46:45 +0200

    make it strict to please old browsers

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-
 // Get Text from URL Paramter
 var text = decodeURIComponent(window.location.href.split("text=")[1]);
 
 // Give Hint to User in case URL parameter is missing
-if (text == "undefined")
+if (text == "undefined") {
     text = "To display Text:\\nAdd it via the text URL parameter";
+}
 
 // Display Text, adding line breaks where there is a \n
 var display = document.getElementById("display");
 display.innerHTML = text.replace("\\n", "<br>");
 
 // Figure out the maximum line length and setting font size accordinglyu
-line_length_max = 0;
-for (let line of text.split("\\n"))
-    if (line.length > line_length_max)
+var line_length_max = 0;
+var lines = text.split("\\n");
+for (var i=0; i<lines.length; i++){
+    var line = lines[i];
+    if (line.length > line_length_max) {
         line_length_max = line.length;
+    }
+}
 
 display.style.fontSize = (150 / line_length_max) + 'vw';
