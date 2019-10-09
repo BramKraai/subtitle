94f436f6497b5ed3235950549de0ae67dc07fc78
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-10-09 13:06:13 +0200

    add help message & error handling

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var url = new URL(window.location.href);
 var text = url.searchParams.get("text");
-var display = document.getElementById("display");
-display.innerHTML = text.replace("\\n", "<br>");
-display.style.fontSize = (100 / text.length) + 'vw';
+
+if (text){
+    var display = document.getElementById("display");
+    display.innerHTML = text.replace("\\n", "<br>");
+    display.style.fontSize = (100 / text.length) + 'vw';
+}
