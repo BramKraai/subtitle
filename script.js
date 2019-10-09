ae830ce1dd40990b7be8c5610133c8e74959bd46
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-10-09 13:18:05 +0200

    pepper debug

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var url = new URL(window.location.href);
 var text = url.searchParams.get("text");
 
-if (text){
+if (!text) {
+    text = "HELLO!";
+}
+
+if (text) {
     var display = document.getElementById("display");
     display.innerHTML = text.replace("\\n", "<br>");
     display.style.fontSize = (100 / text.length) + 'vw';
