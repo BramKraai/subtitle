c5e65418063babfc71780b900f773e9af145e8e0
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-10-09 23:01:00 +0200

    fix silly mistakes

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ var text = decodeURIComponent(window.location.href.split("text=")[1]);
 
 // Give Hint to User in case URL parameter is missing
 if (text == "undefined") {
-    text = "To display Text:\\nAdd it via the text URL parameter";
+    text = "To display Text/Add it via the text URL parameter";
 }
 
-// Display Text, adding line breaks where there is a \n
+// Display Text, adding line breaks where there is a /
 var display = document.getElementById("display");
 display.innerHTML = text.replace(new RegExp("/", 'g'), "<br>").replace(new RegExp("[+]", "g"), " ");
 
 // Figure out the maximum line length and setting font size accordingly
 var line_length_max = 0;
-var lines = text.split("\\n");
+var lines = text.split("/");
 for (var i=0; i<lines.length; i++){
     var line = lines[i];
     if (line.length > line_length_max) {
