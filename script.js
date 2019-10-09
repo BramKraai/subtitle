c2f6b4a601b868f57fc00890a78db204c7343432
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-10-09 13:38:21 +0200

    add support for old browsers

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-var url = new URL(window.location.href);
-var text = url.searchParams.get("text");
 
-if (!text) {
-    text = "HELLO!";
-}
+// Get Text from URL Paramter
+var text = decodeURIComponent(window.location.href.split("text=")[1]);
 
-if (text) {
-    var display = document.getElementById("display");
-    display.innerHTML = text.replace("\\n", "<br>");
-    display.style.fontSize = (100 / text.length) + 'vw';
-}
+// Give Hint to User in case URL parameter is missing
+if (text == "undefined")
+    text = "To display Text:\\nAdd it via the text URL parameter"
+
+// Display Text, adding line breaks where there is a \n
+var display = document.getElementById("display");
+display.innerHTML = text.replace("\\n", "<br>");
+
+// Figure out the maximum line length and setting font size accordinglyu
+line_length_max = 0;
+for (let line of text.split("\\n"))
+    if (line.length > line_length_max)
+        line_length_max = line.length;
+        
+display.style.fontSize = (150 / line_length_max) + 'vw';
