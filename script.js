a85f3e9775c255e867fdc5263f70c0f975a083ed
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-10-09 14:42:14 +0200

    bare bones text scaling

--- script.js
+++ script.js
@@ -20,4 +20,4 @@ for (var i=0; i<lines.length; i++){
     }
 }
 
-display.style.fontSize = Math.min(Math.max(100 / line_length_max, 2), 10) + 'vw';
+display.style.fontSize = (100 / line_length_max) + 'vw';
