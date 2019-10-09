6bbd177315569e0a49ef10d2222672e5c67049b2
Bram Kraaijeveld <bram.kraaijeveld@gmail.com>
2019-10-09 13:42:30 +0200

    fix type

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var text = decodeURIComponent(window.location.href.split("text=")[1]);
 
 // Give Hint to User in case URL parameter is missing
 if (text == "undefined")
-    text = "To display Text:\\nAdd it via the text URL parameter"
+    text = "To display Text:\\nAdd it via the text URL parameter";
 
 // Display Text, adding line breaks where there is a \n
 var display = document.getElementById("display");
@@ -15,5 +15,5 @@ line_length_max = 0;
 for (let line of text.split("\\n"))
     if (line.length > line_length_max)
         line_length_max = line.length;
-        
+
 display.style.fontSize = (150 / line_length_max) + 'vw';
